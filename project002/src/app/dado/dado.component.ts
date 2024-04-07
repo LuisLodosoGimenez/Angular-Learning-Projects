dd4bb0cb9db8d53833f75b1587f317f7609d9339
LuisLodosoGimenez <luislodosogimenez@gmail.com>
2024-04-08 00:12:52 +0200

    project002 finished

--- project002/src/app/dado/dado.component.ts
+++ project002/src/app/dado/dado.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -9,5 +9,7 @@ import { CommonModule } from '@angular/common';
   styleUrl: './dado.component.css'
 })
 export class DadoComponent {
-  valor: number = Math.floor(Math.random() * 6) + 1;
+
+  @Input() valor: string="";
+
 }
