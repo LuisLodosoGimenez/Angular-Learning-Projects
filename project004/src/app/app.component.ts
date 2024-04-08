7679c49b321800cf79928e99d144a4fda32c8250
LuisLodosoGimenez <luislodosogimenez@gmail.com>
2024-04-08 18:23:03 +0200

    project004 second part

--- project004/src/app/app.component.ts
+++ project004/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { SelectornumericoComponent } from './selectornumerico/selectornumerico.component';
@@ -12,9 +12,19 @@ import { FormControl, ReactiveFormsModule} from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  @ViewChild('selector3') selector3!: SelectornumericoComponent;
+  @ViewChild('selector4') selector4!: SelectornumericoComponent;
   
-  myNumberInput: FormControl = new FormControl('');
+  myNumberInput2: FormControl = new FormControl('');
+  myNumberInput4: FormControl = new FormControl('');
+
+  fijarSelector3(valor:number) {
+    this.selector3.fijar(valor);
+  }
+
+  fijarSelector4(valor:number) {
+    this.selector4.fijar(valor);
+  }
 
 
 }
