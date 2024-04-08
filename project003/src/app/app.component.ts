27811509cbf3b5e42693186a3d021b004e5ad109
LuisLodosoGimenez <luislodosogimenez@gmail.com>
2024-04-08 17:03:28 +0200

    project003 finished

--- project003/src/app/app.component.ts
+++ project003/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { CronometroComponent } from './cronometro/cronometro.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CronometroComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project003';
+  mensaje='';
+
+  actualizar(t:number) {
+    this.mensaje = t + '(se actualiza cada 10 segundos)';
+  }
 }
