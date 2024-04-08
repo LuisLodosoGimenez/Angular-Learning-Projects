cc2f2b632ccb04c62e2b5efc312441c32c5b9545
LuisLodosoGimenez <luislodosogimenez@gmail.com>
2024-04-09 00:06:37 +0200

    project008 finished

---
+++ project008/src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  mensaje = ''
+  conversion = '';
+  potenciaNumero = '';
+  cantidad = '';
+
+  formularioConversion = new FormGroup({
+    numerodecimal: new FormControl(''),
+    base: new FormControl('octal'),
+    potencia: new FormControl('2'),
+    largo: new FormControl(true)
+  });
+
+  submit() {
+    this.mensaje=''
+    this.conversion=''
+    this.potenciaNumero=''
+    this.cantidad=''
+    if (this.formularioConversion.value.numerodecimal) {
+      if (this.formularioConversion.value.base == "hexadecimal")
+        this.conversion = parseInt(this.formularioConversion.value.numerodecimal).toString(16);
+      if (this.formularioConversion.value.base == "octal")
+        this.conversion = parseInt(this.formularioConversion.value.numerodecimal).toString(8);
+      if (this.formularioConversion.value.potencia && !(this.formularioConversion.value.potencia == 'No elevar'))
+        this.potenciaNumero = Math.pow(parseInt(this.formularioConversion.value.numerodecimal), parseInt(this.formularioConversion.value.potencia)).toString();
+      if (this.formularioConversion.value.largo)
+        this.cantidad = this.formularioConversion.value.numerodecimal.length.toString();
+    }else this.mensaje = 'AÑADE UN NÚMERO'
+  }
+
+}
