f41e206d5babaf1d91f33f8268e1861f4dcabe6d
LuisLodosoGimenez <luislodosogimenez@gmail.com>
2024-04-09 13:59:15 +0200

    project10 finiched

--- project010/src/app/app.component.ts
+++ project010/src/app/app.component.ts
@@ -1,13 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ReactiveFormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'project010';
+  resultado='';
+
+  formularioContacto = new FormGroup({
+    nombre: new FormControl('', [Validators.required, Validators.minLength(10)]),
+    mail: new FormControl('', [Validators.required, Validators.email]),
+    mensaje: new FormControl('', [Validators.required, Validators.maxLength(500)])
+  });
+
+  submit() {
+    if (this.formularioContacto.valid)
+      this.resultado = "TODOS LOS DATOS SON VÁLIDOS";
+    else
+      this.resultado = "HAY DATOS INVÁLIDOS EN EL FORMULARIO";
+  }
 }
