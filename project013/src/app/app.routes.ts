a5b55c83244b05126e737ddd70d6520cbdf15f5f
LuisLodosoGimenez <luislodosogimenez@gmail.com>
2024-04-09 18:29:46 +0200

    project13 finished

---
+++ project013/src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { TablanroComponent } from './tablanro/tablanro.component';
+
+export const routes: Routes = [{
+  path: "tabla/:nro",
+  component: TablanroComponent
+}];
